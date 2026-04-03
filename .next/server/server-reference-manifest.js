47d390d5823e402b16cc80fd9eb0b52b8c2aba04
shakil <mask.shakill@gmail.com>
2026-04-03 12:07:03 +0600

    remove txt.file

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e1E38jQzjZBEFHoftLntdXFve6pW2ItzJBEyHVpyNrI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"qXfxOm/0k7SOskU0zY6F6969tJ/K94OSSh9WsebeTis=\"}"
